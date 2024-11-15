646cde037c14d02a0874c658406809d4adbfe4c2
ImeneDehimi <imenedehimi8@gmail.com>
2024-11-15 20:33:14 +0100

    link

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,8 +1,8 @@
 import NavBar from "../navbar/NavBar";
 import SideNav from "../sidenav/SideNav";
 import "./Work.css";
-import website1 from "./../../assets/handyman.png"
-import website2 from "./../../assets/scotish.png"
+import website1 from "./../../assets/handyman.png";
+import website2 from "./../../assets/scotish.png";
 import { Link } from "react-router-dom";
 
 const Work = () => {
@@ -16,19 +16,21 @@ const Work = () => {
             <h2>My work</h2>
             <span className="short-line"></span>
             <div className="site site1">
-            <Link target="blank" to={"https://handyman-home-services.netlify.app/"}>
-              <div className="preview">               
-                <img src={website1} alt="" />
-              </div>
+              <Link
+                target="blank"
+                to={"https://handyman-home-services.netlify.app/"}
+              >
+                <div className="preview">
+                  <img src={website1} alt="" />
+                </div>
               </Link>
               <div className="text">
                 <h5>Handyman Home Services</h5>
                 <p>
-                  Handyman home services is a website dedicated
-                  to provide comprehensive home repair and maintenance
-                  solutions. It offers a detailed overview of the services
-                  available, including plumbing, electrical work, building, and
-                  painting.
+                  Handyman home services is a website dedicated to provide
+                  comprehensive home repair and maintenance solutions. It offers
+                  a detailed overview of the services available, including
+                  plumbing, electrical work, building, and painting.
                 </p>
               </div>
             </div>
@@ -36,14 +38,17 @@ const Work = () => {
               <div className="text">
                 <h5>Scottish</h5>
                 <p>
-                 Scottish is a home page for a cafe & restaurant where you can find
-                 different sections displaying information about it.
+                  Scottish is a home page for a cafe & restaurant where you can
+                  find different sections displaying information about it.
                 </p>
               </div>
-              <Link target="blank" to={"https://handyman-home-services.netlify.app/"}>
-              <div className="preview">               
-                <img src={website2} alt="" />
-              </div>
+              <Link
+                target="blank"
+                to={"https://scottish-cafe-restaurant.netlify.app"}
+              >
+                <div className="preview">
+                  <img src={website2} alt="" />
+                </div>
               </Link>
             </div>
           </div>
